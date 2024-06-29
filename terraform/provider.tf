fbe1f7f8b528487f06cc31c49c4faf04a7cc8e49
lokesh <lokeshroyal245@gmail.com>
2024-06-29 23:43:49 +0530

    frontend-deploy

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "devops-project.site-state"
+    key    = "expense-dev-frontend-deploy"
+    region = "us-east-1"
+    dynamodb_table = "devops-project.site-state-locking"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
